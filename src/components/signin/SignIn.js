82851d52cb91fd5878abe84b376a4b7a809554c1
dude1144 <austin.anderson1144@gmail.com>
2019-03-16 16:11:28 -0700

    Updated api url

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -41,7 +41,7 @@ validateLogin = async e =>
   e.preventDefault();
   this.setState({request : JSON.stringify({email : this.state.email.toLowerCase(), password : this.state.password})});
 
-  const response = await fetch('https://dashboard.heroku.com/apps/cs160bankingapp-api/api/validateLogin', {
+  const response = await fetch('https://cs160bankingapp-api.heroku.com/api/validateLogin', {
   method: 'POST',
   headers: {'Content-type': 'application/json',},
   body: JSON.stringify({ email: this.state.email.toLowerCase(), password : this.state.password }),
