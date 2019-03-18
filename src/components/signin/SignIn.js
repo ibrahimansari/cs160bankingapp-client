bfaa91fd1a987b965af40407aabc6917fb198db6
bbdavidbb <bbdavidbb@gmail.com>
2019-03-17 18:39:39 -0700

    fixed typo

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -41,7 +41,7 @@ validateLogin = async e =>
   e.preventDefault();
   this.setState({request : JSON.stringify({email : this.state.email.toLowerCase(), password : this.state.password})});
 
-  const response = await fetch('https://cs160bankingapp-api.herokuapp.comapi/validateLogin', {
+  const response = await fetch('https://cs160bankingapp-api.herokuapp.com/api/validateLogin', {
   method: 'POST',
   mode: "cors",
   headers: {'Content-type': 'application/json',},
