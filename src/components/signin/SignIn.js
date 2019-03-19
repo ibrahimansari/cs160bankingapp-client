e66b44be5a5faa53a876b386df2e5b7208e564ac
samesmaeili <32761208+samesmaeili@users.noreply.github.com>
2019-03-18 21:00:31 -0700

    Update SignIn.js

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -66,7 +66,7 @@ class SignIn extends Component {
   e.preventDefault();
   this.setState({request : JSON.stringify({email : this.state.email.toLowerCase(), password : this.state.password})});
 
-  const response = await fetch('https://cs160bankingapp-api.herokuapp.com/v', {
+  const response = await fetch('https://cs160bankingapp-api.herokuapp.com/', {
   method: 'POST',
   mode: "cors",
   headers: {'Content-type': 'application/json',},
