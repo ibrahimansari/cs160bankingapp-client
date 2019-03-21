5b326a3091d1646521878c638f0d82836e423306
samesmaeili <32761208+samesmaeili@users.noreply.github.com>
2019-03-21 01:45:29 -0700

    Update SignIn.js

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -49,19 +49,27 @@ class SignIn extends Component {
   body: JSON.stringify({ email: this.state.email.toLowerCase(), password : this.state.password }),
   });
 
-  const body = await response;
+  const body = await response.text();
     console.log(body["value"]);
     console.log(body.value);
     console.log(body);
    
     console.log(" hello");
+    
+   var  a = JSON.stringify(body);
+    console.log(a["value"]);
+    console.log(a.value);
+    console.log("hellohh");
+
    
   //console.log(body);
     
   //  console.log(this.state.respone);
    
   this.setState({response : body});
-        
+    console.log(JSON.stringify(state));
+
+    
     if(this.state.response === 'Valid Login1')    //1 represents customer
   {
     window.location.href = 'https://cs160bankingapp.herokuapp.com/accountdashboard';      //navigate to customer page
