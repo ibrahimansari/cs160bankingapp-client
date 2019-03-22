e94b0a3004a200722ed8221c9502773971bd55b1
bbdavidbb <bbdavidbb@gmail.com>
2019-03-22 13:01:46 -0700

    Delete Another import

--- src/components/atmlocator/AtmLocator.js
+++ src/components/atmlocator/AtmLocator.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-import MainNavbar from '../components/navbar/MainNavbar';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
