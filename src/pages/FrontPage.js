364d8c110d5d6bfa564c1f5139f2f9b48f86e4f8
bbdavidbb <bbdavidbb@gmail.com>
2019-04-22 15:04:54 -0700

    AtmLocatorPage Null fix

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -4,6 +4,12 @@ import Background from '../components/background/Background';
 import './styles/FrontPage.css';
 
 class FrontPage extends Component {
+
+  componentDidMount() {
+    if(this.props.context.isSignedIn !== "true" || this.props.context.isSignedIn !== "false"){
+      this.props.context.updateIsSignedIn("false"); // fix a null exception
+    }
+  }
   render()  {
     const { context } = this.props;
     return (
