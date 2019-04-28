1c4b5743592bd264aa90d783684ce0dd4f042990
Ibrahim Ansari <ibrahim@Ibrahims-MacBook-Pro.local>
2019-04-28 15:44:53 -0700

    new maps bs

--- src/pages/AtmLocatorPage.js
+++ src/pages/AtmLocatorPage.js
@@ -1,18 +1,32 @@
-import React, { Component } from "react";
-import MainNavbar from '../components/navbar/MainNavbar';
-import AtmLocator from '../components/atmlocator/AtmLocator';
 
-class AtmLocatorPage extends Component {
-  render()  {
-    const { context } = this.props;
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+const divStyle = {
+  backgroundColor: "#4e74a6",
+  padding: "50px",
+  margin: "50px",
+  marginTop: "0px"
+};
+ 
+export class MapContainer extends React.Component {
+  render() {
     return (
-      <div>
-        <AtmLocator
-          context = {context}
-        />
-      </div>
+        <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            
+        </InfoWindow>
+      </Map>
     );
   }
 }
-
-export default AtmLocatorPage;
+ 
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAccPI1e6kwhlWF2XANTtIBgF1k1Uf32D0'
+})(MapContainer)
