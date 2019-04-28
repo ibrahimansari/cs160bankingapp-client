3fd8009c96251ef726d278037bd3c313c1e6e3de
Ibrahim Ansari <ibrahim@Ibrahims-MacBook-Pro.local>
2019-04-28 15:18:43 -0700

    key changed

--- src/components/atmlocator/AtmLocator.js
+++ src/components/atmlocator/AtmLocator.js
@@ -42,7 +42,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const AtmLocator = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyASFL9qZEPuMTlSZCE84yFElmttyDa5YZU",
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
